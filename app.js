37a5caf7c99368be02aa6af280e2c03278140ea4
Robson <robson.lsilva36@gmail.com>
2025-01-17 00:35:38 -0300

    function sortearAmigo Implemented.

--- app.js
+++ app.js
@@ -41,3 +41,32 @@ function atualizarListaDeAmigos(){
         listaAmigos.appendChild(lista);  
     } 
 }
+
+function sortearAmigo(){
+    /* 
+    Math.random(): 
+        Gera um número decimal aleatório entre 0 (inclusive) e 1 (exclusivo).
+        Por exemplo, pode gerar algo como 0.375, 0.890, etc.
+
+    Math.random() * listaDeNomes.length: 
+        Multiplicamos o valor gerado por Math.random() pelo comprimento do array (nomes.length), 
+        para garantir que o valor aleatório esteja dentro do intervalo dos índices do array. 
+        No caso, listaDeNomes.length retorna o número de elementos do array.
+
+    Math.floor(): 
+        Arredonda o número gerado para o menor número inteiro. 
+        Isso garante que o índice será um número inteiro válido, adequado para acessar os elementos do array. 
+        Por exemplo, Math.floor(3.874) vai resultar em 3.
+    */ 
+
+    if (listaDeNomes.length > 0) {
+        let indiceSorteado = Math.floor(Math.random() * listaDeNomes.length);
+        let nomeSorteado = listaDeNomes[indiceSorteado];
+
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = 'O amigo secreto sorteado é: ' + nomeSorteado.charAt(0).toUpperCase() + nomeSorteado.slice(1);
+
+    }
+    
+
+}
