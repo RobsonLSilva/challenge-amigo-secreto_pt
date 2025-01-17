5abccbdceae293e649429a8e3ab159ecf94fed79
Robson <robson.lsilva36@gmail.com>
2025-01-16 23:09:17 -0300

    function adicionarAmigos Implemented.

--- app.js
+++ app.js
@@ -1,2 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 
+let listaDeNomes = [];
+
+function adicionarAmigo(){
+    let campo = document.querySelector('input');
+
+    if (campo.value == '') {
+        alert('Por favor, insira um nome.');
+    } else {
+        listaDeNomes.push(campo.value);
+        console.log(listaDeNomes);
+        limpaCampo();
+    }
+}
+
+function limpaCampo(){
+    let campo = document.querySelector('input');
+    campo.value = '';
+}
